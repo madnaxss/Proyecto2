83ff99754fa5e99f64b7addc003d6c55f4cc64f0
alrula@jviladoms.cat <albertonax04@gmail.com>
2023-12-19 12:13:11 +0100

    He añadido el js para animaciones y cambios de pagina

---
+++ Proyecto2.js
@@ -0,0 +1,20 @@
+function Asignaturas() {
+    window.location.href = "Asignaturas.html"; 
+}
+
+window.onload = function() {
+    const spans = document.querySelectorAll('#Planaprincipal span');
+    const colors = ['#FFA500', '#FFC0CB', '#A52A2A', '#0000FF', '#FF0000', '#FFC0CB', '#FFC0CB', '#A52A2A', '#0000FF', '#FF0000'];
+    let index = 0;
+  
+    function cambiarColores() {
+      spans.forEach(function(span, i) {
+        span.style.color = colors[(index + i) % colors.length];
+      });
+      index = (index + 1) % colors.length;
+    }
+  
+    setInterval(cambiarColores, 500); // Cambiar colores cada 500 milisegundos (0.5 segundos)
+  };
+  
+  
