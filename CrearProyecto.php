76d4d74ce47f71e5f84ad93a340496c86bbbb660
nicolau11 <126073751+nicolau11@users.noreply.github.com>
2024-01-09 11:42:50 +0100

    Add files via upload

---
+++ CrearProyecto.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web</title>
+    <link rel="stylesheet" type="text/css" href="Web.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id="usuario">
+                <div id="fotoperfil">
+
+                </div>
+                <p id="NombreUsuario">
+                    Nombre usuario
+                </p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Acerca de</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section id='crearProyecto'>
+        <div class='formCrear'>
+            <form>
+                <input type="text" id='NombreProyecto' placeholder='Nombre Proyecto'>
+                <button class='btGuardar'><a href="proyectos.php">GUARDAR</a></button>
+            </form>
+        </div>
+    </section>
+</body>
